d3a889443b522fdbc9a4bccd363fd43a3f17f18d
Jakob Guldberg Aaes <jakob1379@gmail.com>
2024-11-22 11:27:04 +0100

    feat: initialized project for installation using uv and a minimal setup with nix for dev
    the project is a successor to my gist https://gist.github.com/jakob1379/25939ebe487653c8f949bcacaa536c7d as it has proved valuable for me in my daily develo
    pment, making me actually want to do logging. being able to just add it as a dependency and setup the root-logger quickly will benefit many projects in the f
    uture.
    
    BREAKING CHANGE:

---
+++ src/sane_rich_logging/__main__.py
@@ -0,0 +1,12 @@
+from sane_rich_logging import setup_logging
+import logging
+
+if __name__ == "__main__":
+    # Test the logging configuration
+    setup_logging()
+
+    logging.debug("This is a debug message.")
+    logging.info("This is an info message.")
+    logging.warning("This is a warning message.")
+    logging.error("This is an error message.")
+    logging.critical("This is a critical message.")
